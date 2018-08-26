b6ddd87eac4595eebcd371965932cbc9c985bc07
Niteen Sapkal <niteen.sapkal@northgateps.com>
2018-08-26 23:11:26 +0530

    Task 1.1 Delivery driver implementation

--- InterviewTest.DriverData.UnitTests/Analysers/FormulaOneAnalyserTests.cs
+++ InterviewTest.DriverData.UnitTests/Analysers/FormulaOneAnalyserTests.cs
@@ -4,22 +4,44 @@ using NUnit.Framework;
 
 namespace InterviewTest.DriverData.UnitTests.Analysers
 {
-	[TestFixture]
-	public class FormulaOneAnalyserTests
-	{
-		[Test]
-		public void ShouldYieldCorrectValues()
-		{
-			var expectedResult = new HistoryAnalysis
-			{
-				AnalysedDuration = new TimeSpan(10, 3, 0),
-				DriverRating = 0.1231m
-			};
+    [TestFixture]
+    public class FormulaOneAnalyserTests
+    {
+        private FormulaOneAnalyser formulaOneAnalyser;
 
-			var actualResult = new FormulaOneAnalyser().Analyse(CannedDrivingData.History);
+        [SetUp]
+        public void Initialize()
+        {
+            formulaOneAnalyser = new FormulaOneAnalyser(new DriverAnalysisCriteria
+            {
+                AllowedStartTime = new TimeSpan(0, 0, 0),
+                AllowedEndTime = new TimeSpan(0, 0, 0),
+                AllowedMaxSpeed = 200m,
+                RatingForExceedingSpeedLimit = 1,
+            });
 
-			Assert.That(actualResult.AnalysedDuration, Is.EqualTo(expectedResult.AnalysedDuration));
-			Assert.That(actualResult.DriverRating, Is.EqualTo(expectedResult.DriverRating).Within(0.001m));
-		}
-	}
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            formulaOneAnalyser = null;
+        }
+
+        [Test]
+        public void ShouldYieldCorrectValues()
+        {
+            var expectedResult = new HistoryAnalysis
+            {
+                AnalysedDuration = new TimeSpan(10, 3, 0),
+                DriverRating = 0.1231m
+            };
+
+            var actualResult = formulaOneAnalyser.Analyse(CannedDrivingData.History);
+
+            Assert.That(actualResult.AnalysedDuration, Is.EqualTo(expectedResult.AnalysedDuration));
+            Assert.That(actualResult.DriverRating, Is.EqualTo(expectedResult.DriverRating).Within(0.001m));
+        }
+    }
 }
--- InterviewTest.DriverData/Analysers/FormulaOneAnalyser.cs
+++ InterviewTest.DriverData/Analysers/FormulaOneAnalyser.cs
@@ -1,14 +1,66 @@
-﻿using System;
+﻿using InterviewTest.DriverData.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace InterviewTest.DriverData.Analysers
 {
-	// BONUS: Why internal?
-	internal class FormulaOneAnalyser : IAnalyser
-	{
-		public HistoryAnalysis Analyse(IReadOnlyCollection<Period> history)
-		{
-			throw new NotImplementedException();
-		}
-	}
+    // BONUS: Why internal?
+    internal class FormulaOneAnalyser : BaseDriverAnalyser, IAnalyser
+    {
+        public FormulaOneAnalyser(DriverAnalysisCriteria driverAnalysisCriteria) : base(driverAnalysisCriteria) { }
+        public HistoryAnalysis Analyse(IReadOnlyCollection<Period> history)
+        {
+            var historyAnalysisResult = new HistoryAnalysis
+            {
+                AnalysedDuration = new TimeSpan(0, 0, 0),
+                DriverRating = 0
+            };
+
+            if (history != null && history.Any())
+            {
+                var analysisList = InitializePeriodAnalysisData(history);
+                var analysedList = AnalyseValidPeriodList(analysisList);
+                analysedList = AnalysisUndocumentedPeriodList(analysedList);
+                historyAnalysisResult = ComputeHistoryAnalysis(analysedList);
+            }
+            return historyAnalysisResult;
+        }
+
+        /// <summary>
+        /// Analyses valid period list for formula one driver
+        /// </summary>
+        /// <param name="periodAnalysisList">Period Analysis List</param>
+        /// <returns>Period Analysis List after analysis</returns>
+        protected override List<PeriodAnalysis> AnalyseValidPeriodList(List<PeriodAnalysis> periodAnalysisList)
+        {
+            if (periodAnalysisList == null && !periodAnalysisList.Any())
+                return periodAnalysisList;
+
+            //skip anything befoe first non-zero speed.
+            periodAnalysisList = periodAnalysisList.SkipWhile(period => period.AverageSpeed <= 0).ToList();
+
+            if (!periodAnalysisList.Any())
+                return periodAnalysisList;
+
+            //get index of last non zero analysis
+            var lastNonZeroPeriodIndex = periodAnalysisList.IndexOf(periodAnalysisList.LastOrDefault(p => p.AverageSpeed > 0));
+
+            periodAnalysisList = periodAnalysisList.Take(lastNonZeroPeriodIndex + 1).ToList();
+
+            foreach (var period in periodAnalysisList)
+            {
+                //duration in seconds
+                period.DurationInSeconds = (decimal)(period.End - period.Start).TotalSeconds;
+                //this is documented period. set flag to false
+                period.IsUndcoumentedPeriod = false;
+
+                //calculate rating
+                period.Rating = period.AverageSpeed > driverAnalysisCriteria.AllowedMaxSpeed ?
+                                        driverAnalysisCriteria.RatingForExceedingSpeedLimit : period.AverageSpeed / driverAnalysisCriteria.AllowedMaxSpeed;
+            }
+            return periodAnalysisList;
+
+        }
+    }
 }
