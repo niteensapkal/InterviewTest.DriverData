39f484f5543542fb3595bc575d26567fee3f71cb
Niteen Sapkal <niteen.sapkal@northgateps.com>
2018-08-27 00:42:15 +0530

    Gateway Driver Analyser implementation

--- InterviewTest.DriverData.UnitTests/Analysers/GetawayDriverAnalyserTests.cs
+++ InterviewTest.DriverData.UnitTests/Analysers/GetawayDriverAnalyserTests.cs
@@ -7,7 +7,28 @@ namespace InterviewTest.DriverData.UnitTests.Analysers
 	[TestFixture]
 	public class GetawayDriverAnalyserTests
 	{
-		[Test]
+        private GetawayDriverAnalyser getawayDriverAnalyser;
+
+        [SetUp]
+        public void Initialize()
+        {
+            getawayDriverAnalyser = new GetawayDriverAnalyser(new DriverAnalysisCriteria
+            {
+                AllowedStartTime = new TimeSpan(13, 0, 0),
+                AllowedEndTime = new TimeSpan(14, 0, 0),
+                AllowedMaxSpeed = 80m,
+                RatingForExceedingSpeedLimit = 1,
+            });
+
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            getawayDriverAnalyser = null;
+        }
+        [Test]
 		public void ShouldYieldCorrectValues()
 		{
 			var expectedResult = new HistoryAnalysis
@@ -16,7 +37,7 @@ namespace InterviewTest.DriverData.UnitTests.Analysers
 				DriverRating = 0.1813m
 			};
 
-			var actualResult = new GetawayDriverAnalyser().Analyse(CannedDrivingData.History);
+			var actualResult = getawayDriverAnalyser.Analyse(CannedDrivingData.History);
 
 			Assert.That(actualResult.AnalysedDuration, Is.EqualTo(expectedResult.AnalysedDuration));
 			Assert.That(actualResult.DriverRating, Is.EqualTo(expectedResult.DriverRating).Within(0.001m));
--- InterviewTest.DriverData/Analysers/GetawayDriverAnalyser.cs
+++ InterviewTest.DriverData/Analysers/GetawayDriverAnalyser.cs
@@ -1,14 +1,94 @@
-﻿using System;
+﻿using InterviewTest.DriverData.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace InterviewTest.DriverData.Analysers
 {
-	// BONUS: Why internal?
-	internal class GetawayDriverAnalyser : IAnalyser
-	{
-		public HistoryAnalysis Analyse(IReadOnlyCollection<Period> history)
-		{
-			throw new NotImplementedException();
-		}
-	}
+    // BONUS: Why internal?
+    internal class GetawayDriverAnalyser : BaseDriverAnalyser, IAnalyser
+    {
+        public GetawayDriverAnalyser(DriverAnalysisCriteria driverAnalysisCriteria) : base(driverAnalysisCriteria) { }
+        public HistoryAnalysis Analyse(IReadOnlyCollection<Period> history)
+        {
+            var historyAnalysisResult = new HistoryAnalysis
+            {
+                AnalysedDuration = new TimeSpan(0, 0, 0),
+                DriverRating = 0
+            };
+
+            if (history != null && history.Any())
+            {
+                var analysisList = InitializePeriodAnalysisData(history);
+                var analysedList = AnalyseValidPeriodList(analysisList);
+                analysedList = AnalysisUndocumentedPeriodList(analysedList);
+                historyAnalysisResult = ComputeHistoryAnalysis(analysedList);
+            }
+            return historyAnalysisResult;
+        }
+
+        /// <summary>
+        /// Analyses valid period list for Getway driver
+        /// </summary>
+        /// <param name="periodAnalysisList">Period Analysis List</param>
+        /// <returns>Period Analysis List after analysis</returns>
+        protected override List<PeriodAnalysis> AnalyseValidPeriodList(List<PeriodAnalysis> periodAnalysisList)
+        {
+            if (periodAnalysisList == null && !periodAnalysisList.Any())
+                return periodAnalysisList;
+
+            //skip anything befoe first non-zero speed.
+            periodAnalysisList = periodAnalysisList.SkipWhile(period => period.AverageSpeed <= 0).ToList();
+
+            if (!periodAnalysisList.Any())
+                return periodAnalysisList;
+
+            //get index of last non zero analysis
+            var lastNonZeroPeriodIndex = periodAnalysisList.IndexOf(periodAnalysisList.LastOrDefault(p => p.AverageSpeed > 0));
+            periodAnalysisList = periodAnalysisList.Take(lastNonZeroPeriodIndex + 1).ToList();
+
+            if (!periodAnalysisList.Any())
+                return periodAnalysisList;
+
+
+            //remove if analysed end time is before the allowed start time
+            periodAnalysisList.RemoveAll(x => x.End.Hour < driverAnalysisCriteria.AllowedStartTime.Hours);
+
+            if (!periodAnalysisList.Any())
+                return periodAnalysisList;
+
+            //remove if analysed start time is after the allowed end time
+            periodAnalysisList.RemoveAll(x => x.Start.Hour > driverAnalysisCriteria.AllowedEndTime.Hours);
+
+            if (!periodAnalysisList.Any())
+                return periodAnalysisList;
+
+            foreach (var period in periodAnalysisList)
+            {
+                //reset start and end time
+                if (period.Start.Hour < driverAnalysisCriteria.AllowedStartTime.Hours)
+                {
+                    //if analysed start time is before allowed start time reset it to allowed start time
+                    period.Start = new DateTimeOffset(period.Start.Year, period.Start.Month,
+                        period.Start.Day, driverAnalysisCriteria.AllowedStartTime.Hours, driverAnalysisCriteria.AllowedStartTime.Minutes, driverAnalysisCriteria.AllowedStartTime.Seconds, 0, TimeSpan.Zero);
+                }
+                if (period.End.Hour > driverAnalysisCriteria.AllowedEndTime.Hours)
+                {
+                    //if analysed end time is after allowed end time reset it to allowed end time
+                    period.End = new DateTimeOffset(period.End.Year, period.End.Month,
+                        period.End.Day, driverAnalysisCriteria.AllowedEndTime.Hours, driverAnalysisCriteria.AllowedEndTime.Minutes, driverAnalysisCriteria.AllowedEndTime.Seconds, 0, TimeSpan.Zero);
+                }
+                //duration in seconds
+                period.DurationInSeconds = (decimal)(period.End - period.Start).TotalSeconds;
+                //this is documented period. set flag to false
+                period.IsUndcoumentedPeriod = false;
+
+                //calculate rating
+                period.Rating = period.AverageSpeed > driverAnalysisCriteria.AllowedMaxSpeed ?
+                                        driverAnalysisCriteria.RatingForExceedingSpeedLimit : period.AverageSpeed / driverAnalysisCriteria.AllowedMaxSpeed;
+            }
+            return periodAnalysisList;
+
+        }
+    }
 }
