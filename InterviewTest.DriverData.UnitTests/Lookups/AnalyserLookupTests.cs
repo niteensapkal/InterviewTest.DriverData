c329762974a3f9d94d7b61e095d675c97729c388
Niteen Sapkal <niteen.sapkal@northgateps.com>
2018-08-27 15:47:39 +0530

    Implementation for Task 2

---
+++ InterviewTest.DriverData.UnitTests/Lookups/AnalyserLookupTests.cs
@@ -0,0 +1,79 @@
+﻿using InterviewTest.DriverData.Analysers;
+using NUnit.Framework;
+using System;
+
+namespace InterviewTest.DriverData.UnitTests.Lookups
+{
+    [TestFixture]
+    public class AnalyserLookupTests
+    {
+
+        [Test]
+        public void ShouldReturnDeliveryDriverAnalyserForValidInput()
+        {
+            // Arrange
+            var analyserType = "delivery";
+
+            // Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+
+            // Assert
+            Assert.IsInstanceOf(typeof(DeliveryDriverAnalyser), analyser);
+        }
+
+        
+
+        [Test]
+        public void ShouldReturnFormulaOneDriverAnalyserForValidInput()
+        {
+            // Arrange
+            var analyserType = "formulaone";
+
+            // Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+
+            // Assert
+            Assert.IsInstanceOf(typeof(FormulaOneAnalyser), analyser);
+        }
+
+
+        [Test]
+        public void ShouldReturnGetawayDriverAnalyserForValidInput()
+        {
+            // Arrange
+            var analyserType = "getaway";
+
+            // Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+
+            // Assert
+            Assert.IsInstanceOf(typeof(GetawayDriverAnalyser), analyser);
+        }
+
+       
+
+        [Test]
+        public void ShouldThrowExceptionForEmptyInput()
+        {
+            // Arrange
+            var analyserType = "";
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => AnalyserLookup.GetAnalyser(analyserType));
+        }
+
+        [Test]
+        public void ShouldThrowExceptionForNullInput()
+        {
+            Assert.Throws<ArgumentNullException>(() => AnalyserLookup.GetAnalyser(null));
+        }
+
+        [Test]
+        public void ShouldThrowExceptionForInvalidInput()
+        {
+            var analyserType = "driver";
+
+            Assert.Throws<ArgumentException>(() => AnalyserLookup.GetAnalyser(analyserType));
+        }
+    }
+}
--- InterviewTest.DriverData/AnalyserLookup.cs
+++ InterviewTest.DriverData/AnalyserLookup.cs
@@ -1,20 +1,86 @@
 using System;
 using InterviewTest.DriverData.Analysers;
+using System.Collections.Generic;
 
 namespace InterviewTest.DriverData
 {
-	public static class AnalyserLookup
-	{
-		public static IAnalyser GetAnalyser(string type)
-		{
-			switch (type)
-			{
-				case "friendly":
-					return new FriendlyAnalyser();
+    public static class AnalyserLookup
+    {
+        /// <summary>
+        /// Get Analyser
+        /// </summary>
+        /// <param name="analyserType">AnalyserType</param>
+        /// <returns>IAnalyser</returns>
+        public static IAnalyser GetAnalyser(string analyserType)
+        {
+            if (string.IsNullOrEmpty(analyserType))
+                throw new ArgumentNullException(nameof(analyserType), "Null or Empty analyser exception");
 
-				default:
-					throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");
-			}
-		}
-	}
+            if (!analyserList.ContainsKey(analyserType))
+                throw new ArgumentException(nameof(analyserType),  "Invalid analyser exception");
+
+            return analyserList[analyserType]();
+        }
+
+        /// <summary>
+        /// Readonly collection of available analysers
+        /// </summary>
+        private static IReadOnlyDictionary<string, Func<IAnalyser>> analyserList =
+                    new Dictionary<string, Func<IAnalyser>>
+                {
+                    { "delivery", DeliveryDriverAnalyser },
+                    { "friendly", FriendlyAnalyser },
+                    { "formulaone", FormulaOneAnalyser },
+                    { "getaway", GetawayDriverAnalyser }
+                };
+
+        private static DeliveryDriverAnalyser DeliveryDriverAnalyser()
+        {
+            var driverConfiguration = new DriverAnalysisCriteria()
+            {
+                AllowedStartTime = new TimeSpan(9, 0, 0),
+                AllowedEndTime = new TimeSpan(17, 0, 0),
+                AllowedMaxSpeed = 30m,
+                RatingForExceedingSpeedLimit = 0.0m,
+                IsPenaltyApplicable = true,
+                Penalty = 0.5m
+            };
+            return new DeliveryDriverAnalyser(driverConfiguration);
+        }
+
+        private static FriendlyAnalyser FriendlyAnalyser()
+        {
+            return new FriendlyAnalyser();
+        }
+
+        private static FormulaOneAnalyser FormulaOneAnalyser()
+        {
+            var driverConfiguration = new DriverAnalysisCriteria()
+            {
+                AllowedStartTime = TimeSpan.Zero,
+                AllowedEndTime = TimeSpan.Zero,
+                AllowedMaxSpeed = 200m,
+                RatingForExceedingSpeedLimit = 1.0m,
+                IsPenaltyApplicable = true,
+                Penalty = 0.5m
+            };
+
+            return new FormulaOneAnalyser(driverConfiguration);
+        }
+
+        private static GetawayDriverAnalyser GetawayDriverAnalyser()
+        {
+            var driverAnalysisCriteria = new DriverAnalysisCriteria()
+            {
+                AllowedStartTime = new TimeSpan(13, 0, 0),
+                AllowedEndTime = new TimeSpan(14, 0, 0),
+                AllowedMaxSpeed = 80m,
+                RatingForExceedingSpeedLimit = 1.0m,
+                IsPenaltyApplicable = true,
+                Penalty = 0.5m
+            };
+
+            return new GetawayDriverAnalyser(driverAnalysisCriteria);
+        }
+    }
 }
